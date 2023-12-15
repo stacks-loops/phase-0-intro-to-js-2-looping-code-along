358f566ca8f8e8979df463a7443fd0949f1342cc
Parker <p.cronin.connolly@gmail.com>
2023-12-14 19:57:20 -0500

    that wa awesome, while loop 1st try

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+
+function writeCards(arrayName, eventName) {
+    const newArray = []
+    for (let i = 0; i < arrayName.length; i++) {
+
+     newArray.push (`Thank you, ${arrayName[i]}, for the wonderful ${eventName} gift!`)
+    }
+return newArray
+}
+
+writeCards(['Gudalupe', 'Ollie', 'Aki'])
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer)
+        integer--
+    }
+     
+}
